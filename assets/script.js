6c8e92289c9beef35348a58a176c5de9359eb9e6
Nadirah Jama <Nadirahjama@gmail.com>
2022-02-06 16:07:48 -0600

    created css/html/and javascript files

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+// my code here //
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
